cb6e41fb6d89a1e61787fbbd972381a78824a5c1
ThomasCoder223 <56737624+ThomasCoder223@users.noreply.github.com>
2022-10-25 18:53:44 +0100

    Update Pumpkin_Code.ino

--- Pumpkin_Code.ino
+++ Pumpkin_Code.ino
@@ -3,9 +3,9 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
-Servo servo_8;
-Servo servo_9;
-Servo servo_10;
+Servo servo_8; //left eye
+Servo servo_9; //mouth
+Servo servo_10; //right eye
 float getDistance(int trig,int echo){
     pinMode(trig,OUTPUT);
     digitalWrite(trig,LOW);
@@ -34,11 +34,11 @@ void setup() {
   servo_9.write(90);
   servo_10.write(76);
   while(1) {
-      if(getDistance(7,6) < 50){
-          for(int count=0;count<10;count++){
-              digitalWrite(5,1);
+      if(getDistance(7,6) < 50){ // If trigered
+          for(int count=0;count<10;count++){ //Repeat 10 times
+              digitalWrite(5,1); //Turn on LEDs
               digitalWrite(4,1);
-              servo_8.write(100);
+              servo_8.write(100);//Servo motor dance...
               _delay(0.2);
               servo_9.write(100);
               _delay(0.2);
@@ -51,14 +51,14 @@ void setup() {
               servo_10.write(65);
               _delay(0.2);
           }
-          for(int count2=0;count2<10;count2++){
+          for(int count2=0;count2<10;count2++){ //Faster end loop
               servo_8.write(80);
               servo_9.write(80);
-              servo_10.write(65);
+              servo_10.write(80);
               _delay(0.1);
               servo_8.write(100);
               servo_9.write(100);
-              servo_10.write(87);
+              servo_10.write(100);
               _delay(0.1);
           }
           digitalWrite(4,0);
